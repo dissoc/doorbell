dcec13e681ce894dcb477c2809627188fc3cbf83
Justin Bishop <mail@dissoc.me>
2024-02-19 21:53:47 -0600

    added Makefile.am and added details to readme

--- doorbell.scm
+++ doorbell.scm
@@ -13,23 +13,23 @@
     #:unwind? #t))
 
 (define (dinger host port delay verbose?)
-  (begin (display (string-append "dinging" host ":" port))
+  (begin (display (string-append "dinging" host ":" (number->string port)))
          (when verbose?
-           (display (string-append "Knocking port: " port)))
+           (display (string-append "Knocking port: " (number->string port))))
          (let* ((s (socket PF_INET SOCK_STREAM 0))
                 (flags (fcntl s F_GETFL)))
            ;; use nonblocking sockets
            (fcntl s F_SETFL (logior O_NONBLOCK flags))
-           (connect s AF_INET (inet-pton AF_INET host) (string->number port))
+           (connect s AF_INET (inet-pton AF_INET host) port)
            (when verbose?
-             (display (string-append "Knocked port: " port)))
-           (usleep (* 1000 (string->number delay)))
+             (display (string-append "Knocked port: " (number->string port))))
+           (usleep (* 1000  delay))
            ;; shutdown the socket for receiving and transmitting (2)
            ;; if too short of a delay socket transmission might fail?
            ;; untested.
            ;; NOTE: Consider making shutdown only for receiving
            (when verbose?
-             (display (string-append "Delayed : " delay)))
+             (display (string-append "Delayed : " (number->string delay))))
            (close-port s))))
 
 (define (ring-doorbell host ports delay verbose?)
@@ -40,9 +40,9 @@
                                         " doorbell ports: "
                                         ports
                                         " with delay of "
-                                        delay)))
+                                        (number->string delay))))
               (dinger host
-                      p
+                      (string->number p)
                       delay
                       verbose?))
             (string-split ports #\,)))
@@ -64,15 +64,15 @@ doorbell -h 192.168.1.1 -t 400,500,600 [options]
                         (host    (single-char #\h) (value #t))
                         (tcp     (single-char #\t) (value #t))
                         (delay   (single-char #\d) (value #t))
-                        (verbose (value #f))
+                        (verbose (value #t))
                         (exec    (single-char #\e) (value #t))))
          (options (getopt-long args option-spec))
          (help-wanted (option-ref options 'help #f))
-         (delay (option-ref options 'delay #f))
-         (tcp-port (option-ref options 'tcp #f))
+         (knock-delay (option-ref options 'delay #f))
+         (tcp-ports (option-ref options 'tcp #f))
          (host (option-ref options 'host #f))
          (exec (option-ref options 'exec #f))
-         (verbose? (option-ref options 'verbose #f))
+         (verbose? (option-ref options 'verbose #t))
          (version-wanted (option-ref options 'version #f)))
     (newline)
     (if (or version-wanted
@@ -81,18 +81,18 @@ doorbell -h 192.168.1.1 -t 400,500,600 [options]
         (begin
 
           (if version-wanted
-              (display "doorbell version 0.1\n"))
+              (display "doorbell version 0.2\n"))
           (if help-wanted
               (display help)))
 
         ;; must have ports and host
-        (if (and tcp-port host)
+        (if (and tcp-ports host)
             (begin
               (ring-doorbell host
-                             tcp-port
-                             (if (nil? delay)
-                                 100
-                                 (string->number delay))
+                             tcp-ports
+                             (if (string? knock-delay)
+                                 (string->number knock-delay)
+                                 200)
                              verbose?)
               (display (string-append "ringing the doorbell for " host)))
             (begin
